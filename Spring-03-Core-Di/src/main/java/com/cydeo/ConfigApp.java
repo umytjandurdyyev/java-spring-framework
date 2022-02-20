5c1abb1711794f66f7d6c96ba2deba963e74bc03
umytjandurdyyev <udurdiyev@mail.ru>
2022-02-20 18:15:54 +0100

    dependency injection

---
+++ Spring-03-Core-Di/src/main/java/com/cydeo/ConfigApp.java
@@ -0,0 +1,11 @@
+package com.cydeo;
+
+import lombok.Data;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+@ComponentScan
+public class ConfigApp {
+}
