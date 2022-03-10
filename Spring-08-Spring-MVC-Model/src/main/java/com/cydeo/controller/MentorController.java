a8a8af7effac35cdca5ce51725acfcfa18b8cfa1
umytjandurdyyev <udurdiyev@mail.ru>
2022-03-10 17:57:05 +0100

    mentor task

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/MentorController.java
@@ -1,4 +1,25 @@
 package com.cydeo.controller;
 
+import com.cydeo.enums.Gender;
+import com.cydeo.model.Mentor;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class MentorController {
+
+    @RequestMapping("mentor")
+    public String mentorList(Model model){
+//        Mentor mentor = new Mentor("Mike","Smith",35, Gender.MALE);
+        List<Mentor> mentorList = new ArrayList<Mentor>();
+        mentorList.add(new Mentor("Mike","Smith",35, Gender.MALE));
+        mentorList.add(new Mentor("Tom","Hanks",55, Gender.MALE));
+        mentorList.add(new Mentor("Amy","White",25, Gender.FEMALE));
+        model.addAttribute("mentors",mentorList);
+        return "/mentor/mentor-list";
+    }
 }
--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/model/Mentor.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/model/Mentor.java
@@ -1,4 +1,14 @@
 package com.cydeo.model;
 
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Mentor {
+    private String firstname;
+    private String lastname;
+    private int age;
+    private Gender gender;
 }
