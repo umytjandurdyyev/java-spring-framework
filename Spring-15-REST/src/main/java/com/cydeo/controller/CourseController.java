502a15d62846a39b0cd4d1b939e60bea0a7315dd
umytjandurdyyev <udurdiyev@mail.ru>
2022-06-05 13:18:51 +0200

    first api end point created

---
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,25 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses(){
+        List<CourseDTO> list = courseService.getCourses();
+        return list;
+    }
+}
