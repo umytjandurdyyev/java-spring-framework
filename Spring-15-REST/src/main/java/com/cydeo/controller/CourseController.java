988c5ee31a5e0f2af270577d6c687d2ba6f6b6b0
umytjandurdyyev <udurdiyev@mail.ru>
2022-06-05 13:51:29 +0200

    post,put,delete methods created

--- Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -2,13 +2,11 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.CourseDTO;
 import com.cydeo.service.CourseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestController // @Controller + @ResponseBody
 @RequestMapping("/courses")
 public class CourseController {
     private final CourseService courseService;
@@ -17,9 +15,56 @@ public class CourseController {
         this.courseService = courseService;
     }
 
+    /*
+    GET
+     */
+    //localhost:8080/courses
     @GetMapping
     public List<CourseDTO> getAllCourses(){
         List<CourseDTO> list = courseService.getCourses();
         return list;
+    }
+
+    //localhost:8080/courses/3 or localhost:8080/courses/:id
+    @GetMapping("{id}")
+    public CourseDTO getCourseById(@PathVariable("id") Long courseId){
+        return courseService.getCourseById(courseId);
+    }
+
+    //localhost:8080/courses/category/React
+    @GetMapping("category/{name}")
+    public List<CourseDTO> getCourseByCategory(@PathVariable("name") String category){
+        return courseService.getCoursesByCategory(category);
+    }
+
+    /*
+    POST
+     */
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO course){
+        return courseService.createCourse(course);
+    }
+
+    /*
+    PUT
+     */
+    //localhost:8080/courses/3
+    @PutMapping("{id}")
+    public void updateCourse(@PathVariable("id") Long courseId, @RequestBody CourseDTO course){
+        courseService.updateCourse(courseId,course);
+    }
+
+    /*
+    DELETE
+     */
+    //localhost:8080/courses/3
+    @DeleteMapping("{id}")
+    public void deleteCourseId(@PathVariable("id") Long courseId){
+        courseService.deleteCourseById(courseId);
+    }
+
+    @DeleteMapping
+    void deleteCourses(){
+        courseService.deleteCourses();
     }
 }
