350b8213777e6be578d0353eced374e33b73209b
umytjandurdyyev <udurdiyev@mail.ru>
2022-06-07 21:35:29 +0200

    rest api

--- Spring-16-1-REST-Jackson/src/main/java/com/cydeo/entity/Account.java
+++ Spring-16-1-REST-Jackson/src/main/java/com/cydeo/entity/Account.java
@@ -1,6 +1,9 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,9 +15,12 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @Table(name = "account_details")
+//@JsonIgnoreProperties(value = {"state","postalCode"},ignoreUnknown = true)
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"},ignoreUnknown = true)
 public class Account extends BaseEntity {
 
     private String name;
+    @JsonIgnore
     private String address;
     private String country;
     private String state;
@@ -27,6 +33,7 @@ public class Account extends BaseEntity {
     private UserRole role = UserRole.USER;
 
     @OneToOne(mappedBy = "account")
+    @JsonBackReference // is the back part of reference - it will be omitted from serialization
     private User user;
 
 }
