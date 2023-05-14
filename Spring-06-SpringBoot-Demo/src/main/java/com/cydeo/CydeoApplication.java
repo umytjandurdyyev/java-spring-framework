513742c74865df286af04ee7d548c8abc65b85cb
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-05-14 14:46:16 +0200

    Spring Boot Demo

--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/CydeoApplication.java
@@ -5,23 +5,19 @@ import com.cydeo.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class CydeoApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		Comment comment = new Comment();
+		comment.setAuthor("Johnson");
+		comment.setText("Spring framework");
 
-        //        SpringApplication.run(CydeoApplication.class, args);
-
-        Comment comment = new Comment();
-        comment.setAuthor("Johnson");
-        comment.setText("Spring Framework");
-
-        ApplicationContext context = SpringApplication.run(CydeoApplication.class, args);
-
-        CommentService cs = context.getBean(CommentService.class);
-
-        cs.publishComment(comment);
-    }
-
+		// Create a container
+		ApplicationContext context = SpringApplication.run(CydeoApplication.class,args);
+		// commentService object
+		context.getBean(CommentService.class).publishComment(comment);
+	}
 }
--- Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -3,30 +3,20 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("prototype")
-//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
-//@Lazy
 public class CommentService {
-
-    @Value("${days}")
-    private String[] days;
-
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
-
+    @Autowired
 //    public CommentService(CommentRepository commentRepository, @Qualifier("commentPushNotificationProxy") CommentNotificationProxy commentNotificationProxy) {
-    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
-
     }
-
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
