99da77841ea247346c349538323883893744ba90
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-05-18 13:43:28 +0200

    Spring MVC Controller

---
+++ Spring-07-Spring-MVC-Controller/src/test/java/com/cydeo/ControllerApplication/Spring07SpringMvcControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ControllerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring07SpringMvcControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
