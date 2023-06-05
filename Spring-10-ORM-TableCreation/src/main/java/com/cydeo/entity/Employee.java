d2aea8660e9f78bf8a4c2103b0940b9d67766dbc
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-06-05 21:52:26 +0200

    create tables

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Employee.java
@@ -0,0 +1,11 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    private Long id;
+    private String name;
+}
