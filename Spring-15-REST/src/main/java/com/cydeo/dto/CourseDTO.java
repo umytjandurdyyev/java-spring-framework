3b2f6901d248934558a61c8c5e455b931ba4aa57
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-06-29 20:39:31 +0200

    POJO classes and interfaces defined

---
+++ Spring-15-REST/src/main/java/com/cydeo/dto/CourseDTO.java
@@ -0,0 +1,23 @@
+package com.cydeo.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CourseDTO {
+
+    private Long id;
+
+
+    private String name;
+
+
+    private String category;
+
+
+    private int rating;
+
+
+    private String description;
+}
