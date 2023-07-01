386d935d7d11914660a70717e7707b18e1faa78f
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-07-01 19:48:15 +0200

    rest jackson

---
+++ Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/Genre.java
@@ -0,0 +1,20 @@
+package com.cydeo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Genre extends BaseEntity {
+
+    private String name;
+
+    @ManyToMany(mappedBy = "genreList")
+    @JsonIgnore
+    private List<Movie> movieList;
+
+}
---
+++ Spring-16-REST-Jackson/src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,31 @@
+package com.cydeo.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_account")
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"},ignoreUnknown = true)
+public class User extends BaseEntity {
+
+    //    @JsonIgnore
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // only execute setters
+    private String password;
+
+    private String username;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_details_id")
+    @JsonManagedReference  // is the forward part of reference - the one that gets serialized normally
+    private Account account;
+
+}
