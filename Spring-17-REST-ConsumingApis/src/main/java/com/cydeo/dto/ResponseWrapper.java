27ce13d6b6f8c1732c8b349d55021a9e5a99b394
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-07-04 20:48:40 +0200

    feign client added

---
+++ Spring-17-REST-ConsumingApis/src/main/java/com/cydeo/dto/ResponseWrapper.java
@@ -0,0 +1,33 @@
+package com.cydeo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ResponseWrapper {
+    /**
+     * Custom Json body
+     */
+
+    private boolean success;
+    private String message;
+    private  Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.success = true;
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.data = data;
+    }
+
+    public ResponseWrapper(String message) {
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+}
