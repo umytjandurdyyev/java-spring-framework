e064818ce75869ed5504485ab1a261904df9f1ff
Umytjan Durdyyev <udurdiyev@mail.ru>
2023-07-17 18:43:29 +0200

    add tag and rsponse defined for user doc

--- Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/UserController.java
+++ Spring-18-REST-OpenApi3/src/main/java/com/cydeo/controller/UserController.java
@@ -2,6 +2,11 @@ package com.cydeo.controller;
 
 import com.cydeo.entity.User;
 import com.cydeo.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-//@Tag(name = "User", description = "User CRUD Operations")
+@Tag(name = "User", description = "User CRUD Operations")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -20,13 +25,13 @@ public class UserController {
     }
 
     @GetMapping("/users")
-//    @Operation(summary = "Read all users")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200", description = "Successfully retrieved users (OK)",
-//                    content = {@Content(mediaType = "application/json")}),
-//            @ApiResponse(responseCode = "400", description = "Something went wrong", content = @Content),
-//            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
-//    })
+    @Operation(summary = "Read all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved users (OK)",
+                    content = {@Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Something went wrong", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content)
+    })
     public List<User> readAllUsers() {
         return userRepository.findAll();
     }
